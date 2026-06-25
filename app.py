7476dd83655f00905fa60380f71b567a2de7c5e4
RealElvince <obuongely@yahoo.com>
2026-06-25 13:39:48 +0300

    added data path

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
 import streamlit as st
 import pandas as pd
 import plotly.express as px
+from pathlib import Path
+
+DATA_PATH = Path("data") / "online_sales_data.csv"
 
 st.set_page_config(
     page_title="Sales Dashboard"
 )
 st.header("Interactive Sales Dashboard")
+
